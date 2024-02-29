dbfbbb5d6eebe11af317080a162941415ef22481
JEESCOBARMA <157499415+JEESCOBARMA@users.noreply.github.com>
2024-02-29 07:37:19 -0600

    Add files via upload

---
+++ semana7/T1/T1_JEEM1309924/T1_JEEM1309924/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string sNombre;
+        string sEdad;
+        string sCarrera;
+        string sCarne;
+
+        Console.Write("Ingrese su nombre: ");
+        sNombre = Console.ReadLine();
+
+        Console.Write("Ingrese su edad: ");
+        sEdad = Console.ReadLine();
+
+        Console.Write("Ingrese su carrera a la que pertenece: ");
+        sCarrera = Console.ReadLine();
+
+        Console.Write("Ingrese su numero de carne: ");
+        sCarne = Console.ReadLine();
+
+        Console.WriteLine("Mi Segundo Programa");
+        Console.WriteLine("Nombre: " + sNombre);
+        Console.WriteLine("Edad: " + sEdad);
+        Console.WriteLine("Carrera: " + sCarrera);
+        Console.WriteLine("Carne: " + sCarne);
+
+        Console.WriteLine("Soy " + sNombre + ", tengo " + sEdad + " años y estudio la carrera de " + sCarrera + ".");
+        Console.WriteLine("Mi numero de Carne es: " + sCarne);
+        Console.ReadKey();
+    }
+}
